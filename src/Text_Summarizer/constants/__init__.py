7083fb2515f9181d160c2b63a239524d9b375cf8
thekaushik-3011 <sallakaushik@gmail.com>
2024-06-12 16:28:05 +0530

    added data ingestion notebook

---
+++ src/Text_Summarizer/constants/__init__.py
@@ -0,0 +1,4 @@
+from pathlib import Path
+
+CONFIG_FILE_PATH = Path(r"C:\\Users\salla\\OneDrive\Desktop\\Coding\\Machine Learning\\Project\summarizer\\config\\config.yaml")
+PARAMS_FILE_PATH = Path(r"C:\\Users\salla\\OneDrive\Desktop\\Coding\\Machine Learning\\Project\summarizer\\params.yaml")
--- src/Text_Summarizer/utils/common.py
+++ src/Text_Summarizer/utils/common.py
@@ -5,26 +5,33 @@ from Text_Summarizer.logging import logger
 from ensure import ensure_annotations
 from box import ConfigBox
 from pathlib import Path
-from typing import Any
+from typing import Any, List
 
 @ensure_annotations
 def read_yaml(path_to_yaml: Path) -> ConfigBox:
     try:
-        with open(path_to_yaml) as yaml_file:
+        with open(path_to_yaml, 'r') as yaml_file:
             content = yaml.safe_load(yaml_file)
             logger.info(f"yaml file: {path_to_yaml} loaded successfully")
             return ConfigBox(content)
     except BoxValueError:
         raise ValueError("yaml file is empty")
     except Exception as e:
-        raise e 
+        logger.error(f"An error occurred while reading the yaml file: {e}")
+        raise e
     
 @ensure_annotations
 def create_directories(path_to_directories: list, verbose=True):
+    if not isinstance(path_to_directories, list):
+        raise TypeError(f"Expected a list, got {type(path_to_directories).__name__}")
+    
     for path in path_to_directories:
-        os.makedirs(path, exists_ok=True)
+        if not isinstance(path, Path):
+            raise TypeError(f"Expected a Path object, got {type(path).__name__}")
+        
+        os.makedirs(path, exist_ok=True)
         if verbose:
-            logger.info(f"created directory at: {path}")
+            logger.info(f"Created directory at: {path}")
 
 @ensure_annotations
 def get_size(path: Path) -> str:
